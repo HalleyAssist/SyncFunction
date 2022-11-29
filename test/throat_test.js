8a4bce14f9c4a3d235d06f664038969b6df23587
Mathew Heard <splitice@users.noreply.github.com>
2022-11-29 13:24:00 +1100

    ThroatQueueFunction becomes cancellable

--- test/throat_test.js
+++ test/throat_test.js
@@ -79,6 +79,65 @@ describe('ThroatQueueFunction', function(){
         expect(count).to.be.eql(10)
 
     })
+    it('should run 5 times only if cancelled from fn', async() => {
+        const tf = ThroatQueueFunction(5)
+
+        let count = 0
+        const deferred = Q.defer()
+        const p = []
+        for(let i = 0; i < 10; i++){
+            p.push(tf(()=>{
+                count++
+                return deferred.promise
+            }))
+        }
+
+        await Q.delay(10)
+
+
+        expect(count).to.be.eql(5)
+        
+        for(const pp of p){
+            pp.cancel()
+        }
+
+        deferred.resolve(true)
+
+        await tf(null)
+        expect(count).to.be.eql(5)
+
+    })
+    it('should run 5 times only if cancelled from running', async() => {
+        const tf = ThroatQueueFunction(5)
+
+        let count = 0
+        const deferred = Q.defer()
+        const p = []
+        for(let i = 0; i < 10; i++){
+            p.push(tf(Q.canceller(async(cancellationState)=>{
+                await cancellationState.promiseWrap(deferred.promise)
+                count++
+            })))
+        }
+
+        await Q.delay(10)
+
+
+        expect(tf.running.length).to.be.eql(5)
+        
+        for(const pp of tf.running){
+            pp.cancel()
+        }
+
+        deferred.resolve(true)
+
+
+        expect(tf.running.length).to.be.eql(5)
+
+        await tf(null)
+        expect(count).to.be.eql(5)
+
+    })
     it('should capture stack trace', async() => {
         async function testFn(){
             const errors = []
