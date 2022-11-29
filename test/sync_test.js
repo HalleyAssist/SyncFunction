a67f7fefe2eff698d8168f4b020d2e35ff673e1f
Mathew Heard <splitice@users.noreply.github.com>
2022-11-29 19:17:04 +1100

    syncfunctions can now be cancelled and add stack trace tests

--- test/sync_test.js
+++ test/sync_test.js
@@ -65,6 +65,31 @@ describe('SyncFunction', function(){
             expect(count).to.be.eql(3)
             expect(timedOut).to.be.true
         })
+        it('should capture stack trace', async() => {
+            async function testFn(){
+                const errors = []
+                const sf = SyncFunction()
+    
+                for(let i = 0; i < 10; i++){
+                    try {
+                        await sf(()=>{
+                            throw new Error('test')
+                        })
+                    } catch(ex) {
+                        errors.push(ex)
+                    }
+                }
+    
+                return errors
+            }
+    
+            const errors = await testFn()
+            expect(errors.length).to.be.eql(10)
+            for(const e of errors){
+                expect(e.stack).to.be.a('string')
+                expect(e.stack).to.contain('testFn')
+            }
+        })
     })
     describe('queue', function(){
         it('should run all functions', async() => {
@@ -91,6 +116,31 @@ describe('SyncFunction', function(){
             expect(await p[1]).to.be.eql(1)
             expect(await p[0]).to.be.eql(0)
             expect(count).to.be.eql(3)
+        })
+        it('should capture stack trace', async() => {
+            async function testFn(){
+                const errors = []
+                const sf = SyncQueue()
+    
+                for(let i = 0; i < 10; i++){
+                    try {
+                        await sf(()=>{
+                            throw new Error('test')
+                        })
+                    } catch(ex) {
+                        errors.push(ex)
+                    }
+                }
+    
+                return errors
+            }
+    
+            const errors = await testFn()
+            expect(errors.length).to.be.eql(10)
+            for(const e of errors){
+                expect(e.stack).to.be.a('string')
+                expect(e.stack).to.contain('testFn')
+            }
         })
     })
 })
